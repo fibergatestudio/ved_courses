434d967c253d7cd27c6b8804955329b0a55b7d24
aksvitpav <aksvitpav@gmail.com>
2020-12-22 10:38:43 +0200

    #1

--- resources/views/front/protocol.blade.php
+++ resources/views/front/protocol.blade.php
@@ -2552,5 +2552,22 @@
 @section('js')
 <script>
     autosize($('.input-textarea'));
+
+    //photo upload btn emulation and uploaded file name update start
+    const photoButtons = document.querySelectorAll('.add-student.add-student_restyle.p-btn-upload');
+    const nearestLable = document.querySelectorAll('.custom-upload-form.p-custom-upload-form');
+    const inputElement = document.querySelectorAll(".eg-input.add-style.base-upload");
+
+    photoButtons.forEach(btn => {
+        btn.addEventListener('click', function () {
+            this.previousElementSibling.click();
+        })
+    })
+
+    for (let i = 0; i < inputElement.length; i++) {
+        inputElement[i].addEventListener("change", function () {
+            inputElement[i].parentNode.innerText = inputElement[i].files[0].name;
+        });
+    }
 </script>
 @endsection
